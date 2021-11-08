05a5054c30d9aa2bb9e45016ad68671446a1707d
Belen Pizarro <belupizarro@hotmail.com>
2021-11-08 20:41:14 -0300

    v2

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Bienvenidos a Meta
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Tu vida más real que nunca
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
